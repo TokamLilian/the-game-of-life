ea285c356609f109e54181b2ad631e8ab4a23f5e
TokamLilian <tokamlilian@gmail.com>
2023-04-29 08:06:53 -0400

    Added a test for the decide function

--- test_.py
+++ test_.py
@@ -1,6 +1,16 @@
 #this the test file for the main program
+from game_of_life import *
 
 def test():
-    pass
+    
+    def test_decide():
+        grid = [ [0, 0, 1],
+                 [0, 1, 1],
+                 [0, 0, 0] ]
+
+        decide(grid)
+
+    test_decide()
+
 
 test()
