bdb6d5cc180d619c7575284ae55bf152981483ec
TokamLilian <tokamlilian@gmail.com>
2023-05-09 12:41:10 -0400

    Added user click to git birth to cells

--- game_of_life.py
+++ game_of_life.py
@@ -28,6 +28,22 @@ def position(x, y):
     pu(); fd(x); lt(90); fd(y); rt(90); pd()
 
 
+def setup_screen(size, square_size, gap):
+    width = size*square_size
+
+    screen = turtle.Screen()
+    screen.setup(width = 0.9, height = 0.9)
+    canvas = screen.getcanvas() 
+    root = canvas.winfo_toplevel()
+    root.overrideredirect(0)
+
+    turtle.delay(0)
+    turtle.hideturtle()
+
+    pu(); goto(-turtle.window_width()/2, turtle.window_height()/2); pd()
+    #pu();draw_box(width, square_size, gap);pd() ##
+
+
 def draw_box(width, square_size, gap):
 #this function draws a box in which the game takes place        
     position(0, square_size)
@@ -69,7 +85,7 @@ def draw(grid, square_size, size, gap):
             else: draw_cell(square_size, "white")
             position(-j*square_size, i*square_size)
 
-    position(-gap/2, gap/2)                         #to place the turtle at the original position on the grid for the next executiion
+    #position(-gap/2, gap/2)                         #to place the turtle at the original position on the grid for the next executiion
 
 
 def get_neighbour(grid, index):
@@ -95,11 +111,9 @@ def get_neighbour(grid, index):
     return neighbours
 
 
-def decide_next(neighbours, cell_index, cell_status, grid):
+def decide_next(neighbours, cell_index, cell_status, grid, size):
 #this function determines if a 0 cell has 3 live neighbours inorder to make it alive
 #this function determines if a cell 1 has less than 2 live cells or greater than three live cells to make it die
-
-    size = len(grid)
 
     alive_neighbours = 0
 
@@ -138,19 +152,36 @@ def decide(grid, square_size, size, gap):
             cell_state = grid[i+row][col]
 
             neighbours = get_neighbour(old_grid, cell_index)
-            new_grid = decide_next(neighbours, cell_index, cell_state, grid)
+            new_grid = decide_next(neighbours, cell_index, cell_state, grid, size)
 
     draw(new_grid, square_size, size, gap)
 
     return new_grid
 
 
-def update_grid(i, j):
+def update_grid(x, y, grid, size, square_size):
 #to update the grid and proceed to next verification
-    print('x', i, 'and y', j) ##
-    grid = []
-    return grid
 
+    # -576 <= x <= (size*sqaure_size) - 576
+    # 380 - (size*square_size) <= y <= 285
+    lx = -576                           #the lower boundary of x on the canvas
+    uy = 285                            #the upper boundary of y on the canvas
+
+    x = abs(lx - x)
+    y = abs(y - uy)
+
+    x_pos = int(x//square_size)         #to know position of the cell
+    y_pos = int(y//square_size)
+
+    i = size - 1 - y_pos                #to get the index of the cell's value in the grid
+    j = x_pos
+
+    #print('x position', x_pos, 'and y position', y_pos)
+    #square = grid[i][j]
+    #print('The square is', square)
+
+    grid[i][j] = 1                      #change the clicked cell to alive
+    
 
 def init():
 #this function begins the program execution
@@ -178,35 +209,20 @@ def init():
           array_size = information[0]
           square_size = information[1]
 
-    screen = turtle.Screen()
-    screen.setup(width = 0.9, height = 0.9)
-    canvas = screen.getcanvas() 
-    root = canvas.winfo_toplevel()
-    root.overrideredirect(0)
-
-    turtle.delay(0)
-    turtle.hideturtle()
     grid = create_array(array_size)
-    pu()
-    goto(-turtle.window_width()/2, turtle.window_height()/2);pd()
-
+    
     size = len(grid)
     #gap = square_size/size 
     #if gap < 1: gap **= -1      #to take the dividend of both values
+    #elif gap == 1: gap = 5      #take an arbitray value
     gap = 10
-    width = size*square_size
-    pu();draw_box(width, square_size, gap);pd() ##
+    
+    setup_screen(size, square_size, gap)
 
     while True:
         grid = decide(grid, square_size, size, gap)
-        turtle.onscreenclick(update_grid, 1)
+        turtle.onscreenclick(lambda x, y: update_grid(x, y, grid, size, square_size))
         turtle.listen()
         #turtle.clear()
 
 init()
-
-def take_click():
-    draw_box(100)
-    turtle.onscreenclick(update_grid, 1)
-    turtle.listen()
-    turtle.done()
