1413b115ca5f98ecd06f14d05453cf68abbb2adc
TokamLilian <tokamlilian@gmail.com>
2023-05-10 14:27:39 -0400

    Now the program can run with a rectangular canvas too

--- game_of_life.py
+++ game_of_life.py
@@ -4,19 +4,19 @@ import turtle
 from turtle import *
 from copy import *
 
-def create_array(size):
+def create_array(lines, columns):
 #this function returns a sizexsize matrix as a two-dimensional array 
 
-    if size < 0: return None
+    if columns <= 0 or lines <= 0: return None
 
-    grid = [None] * size
+    grid = [None] * lines
 
-    for i in range (size):
-        grid[i] = [None] * size
+    for i in range (lines):
+        grid[i] = [None] * columns
 
-    for j in range ( size**2 ):
-        row = j // size
-        col = j % size
+    for j in range ( columns * lines ):
+        row = j // columns
+        col = j % columns
         value = randint(0, 1)
         grid[row][col] = value
 
@@ -28,9 +28,7 @@ def position(x, y):
     pu(); fd(x); lt(90); fd(y); rt(90); pd()
 
 
-def setup_screen(size, square_size, gap):
-    width = size*square_size
-
+def setup_screen():
     screen = turtle.Screen()
     screen.setup(width = 0.9, height = 0.9)
     canvas = screen.getcanvas() 
@@ -70,15 +68,15 @@ def draw_cell(square_size, colour):
     turtle.color("black")
 
 
-def draw(grid, square_size, size, gap):
+def draw(grid, square_size, lines, columns, gap):
 #this function goes through the two-dimensional array representing the grid inorder to proceed with drawing of alive cells or not
 
-    for i in range (size):
+    for i in range (lines):
 
-        for j in range (size):
+        for j in range (columns):
 
-            row = j // size
-            col = j % size
+            row = j // columns
+            col = j % columns
 
             cell_state = grid[i+row][col]
 
@@ -89,25 +87,24 @@ def draw(grid, square_size, size, gap):
             position(-j*square_size, i*square_size)
 
 
-def get_neighbour(grid, index):
+def get_neighbour(grid, index, lines, columns):
 #this function returns the eight neighbours of a cell
-    size = len(grid)
 
-    left = -1 if index % size == 0 else index - 1
-    right = -1 if (index + 1) % size == 0 else index + 1
-    up = -1 if index < size else index - size
-    down = -1 if index > size*(size - 1) else index + size
+    left = -1 if index % columns == 0 else index - 1
+    right = -1 if (index + 1) % columns == 0 else index + 1
+    up = -1 if index < columns else index - columns
+    down = -1 if index > columns*(columns - 1) else index + columns
 
-    diag_lu = -1 if left == -1 or up == -1 else index - size - 1
-    diag_ru = -1 if right == -1 or up == -1 else index - size + 1
-    diag_rd = -1 if right == -1 or down == -1 else index + size + 1
-    diag_ld = -1 if left == -1 or down == -1 else index + size - 1
+    diag_lu = -1 if left == -1 or up == -1 else index - columns - 1
+    diag_ru = -1 if right == -1 or up == -1 else index - columns + 1
+    diag_rd = -1 if right == -1 or down == -1 else index + columns + 1
+    diag_ld = -1 if left == -1 or down == -1 else index + columns - 1
 
     neighbours = []
-    index_max = size ** 2 - 1
+    index_max = (lines * columns) - 1
     for i in [left, right, up, down, diag_lu, diag_ru, diag_rd, diag_ld]:
         if i >= 0 and i <= index_max:
-            neighbours.append(grid[i//size][i%size])
+            neighbours.append(grid[i//columns][i%columns])
 
     return neighbours
 
@@ -134,7 +131,7 @@ def decide_next(neighbours, cell_index, cell_status, grid, size):
     return grid
 
 
-def decide(grid, square_size, size, gap):
+def decide(grid, square_size, lines, columns, gap):
 #this function goes through the grid and inspects if a cell will be set alive or death
 
     """ We need to store the value of the grid in such a way that it is not
@@ -143,30 +140,28 @@ def decide(grid, square_size, size, gap):
 
     old_grid = deepcopy(grid)
     
-    for i in range (size):
+    for i in range (lines):
 
-        for j in range (size):
-            row = j // size
-            col = j % size
+        for j in range (columns):
+            col = j % columns
 
-            cell_index= ((i+row)*size) + col
-            cell_state = grid[i+row][col]
+            cell_index= (i*columns) + col
+            cell_state = grid[i][col]
 
-            neighbours = get_neighbour(old_grid, cell_index)
-            new_grid = decide_next(neighbours, cell_index, cell_state, grid, size)
+            neighbours = get_neighbour(old_grid, cell_index, lines, columns)
+            new_grid = decide_next(neighbours, cell_index, cell_state, grid, columns)
 
-    draw(new_grid, square_size, size, gap)
+    draw(new_grid, square_size, lines, columns, gap)
 
     return new_grid
 
 
-def update_grid(x, y, grid, size, square_size, lx, uy):
+def update_grid(x, y, grid, lines, columns, square_size, lx, uy):
 #to update the grid and proceed to next verification
     
-    length = size * square_size
     uy += square_size                                            #we add square size because after being at that point, the turtle draws a cell upward
-    ly = uy - length
-    if x <= length + lx and x >= lx :                            #ensure that we are within the boundaries of the grid
+    ly = uy - (square_size * lines)
+    if x <= (square_size * columns) + lx and x >= lx :           #ensure that we are within the boundaries of the grid
         if ly <= y and y <= uy:
 
             x = abs(lx - x)
@@ -175,7 +170,7 @@ def update_grid(x, y, grid, size, square_size, lx, uy):
             x_pos = int(x//square_size)         #to know position of the cell
             y_pos = int(y//square_size)
 
-            i = size - 1 - y_pos                #to get the index of the cell's value in the grid
+            i = lines - 1 - y_pos                #to get the index of the cell's value in the grid
             j = x_pos
 
             #print('x position', x_pos, 'and y position', y_pos)
@@ -189,42 +184,61 @@ def init():
 #this function begins the program execution
 
     def get_inputs():
-        try:
-            array_size = int(input('Select dimension: '))
-            square_size = int(input('Enter square size: '))
-            return [array_size, square_size]
-        except:
-            get_inputs() 
+        shape = input('Enter [S] for a square and [R] for a rectangular canvas: ').upper()
+        if shape == 'S':
+            try:
+                array_size = int(input('Select dimension: '))
+                square_size = int(input('Enter square size: '))
+                return [array_size, array_size, square_size]
+            except:
+                get_inputs()
 
-    if len(sys.argv) == 3:
+        elif shape == 'R': 
+            try:
+                lines = int(input('Enter number of lines: '))
+                columns = int(input('Enter number of columns: '))
+                square_size = int(input('Enter square size: '))
+                return [lines, columns, square_size]
+            except:
+                get_inputs()
+
+        else: get_inputs()
+
+    if len(sys.argv) >= 3:
         try:
-            array_size = int(sys.argv[1])
-            square_size = int(sys.argv[2])                            
+            lines = int(sys.argv[1])
+            columns = int(sys.argv[2])
+            square_size = int(sys.argv[3])                            
 
         except:
-            information = get_inputs()
-            array_size = information[0]
-            square_size = information[1]
+            try:
+                lines = columns = int(sys.argv[1])
+                square_size = int(sys.argv[2])
+
+            except:   
+                information = get_inputs()
+                lines = information[0]
+                columns = information[1]
+                square_size = information[2]
 
     else: 
           information = get_inputs()
-          array_size = information[0]
-          square_size = information[1]
+          lines = information[0]
+          columns = information[1]
+          square_size = information[2]
 
-    grid = create_array(array_size)
+    grid = create_array(lines, columns)
     
-    size = len(grid)
     #gap = square_size/size 
     #if gap < 1: gap **= -1      #to take the dividend of both values
     #elif gap == 1: gap = 5      #take an arbitray value
-    gap = 10
+    gap = 10 ##
     
-    boundaries = setup_screen(size, square_size, gap)
+    boundaries = setup_screen()
 
     while True:
-        grid = decide(grid, square_size, size, gap)
-        turtle.onscreenclick(lambda x, y: update_grid(x, y, grid, size, square_size, boundaries[0], boundaries[1]))
+        grid = decide(grid, square_size, lines, columns, gap)
+        turtle.onscreenclick(lambda x, y: update_grid(x, y, grid, lines, columns, square_size, boundaries[0], boundaries[1]))
         turtle.listen()
-        #turtle.clear()
 
 init()
